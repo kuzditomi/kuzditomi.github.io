48ec756fbd4aa876d8663904dc7b0d7d5216f05e
kuzditomi <kuzditomi@gmail.com>
2022-03-26 17:51:17 +0100

    add bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import 'bulma/css/bulma.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello World
+        </h1>
+        <p className="subtitle">
+          Welcome to the site of <strong>kuzditomi</strong>!
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      </div>
+    </section>
   )
 }
 
